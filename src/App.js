3fd9c8f7e3b062aa4992a38bb176be8cf30a8739
Alexander Karren <alexanderkarren@Alexanders-MacBook-Pro.local>
2020-02-18 09:58:31 -0700

    added timer functionality using setTimer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 import Buttons from "./Buttons";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  const [homeName, setHome] = useState("Lions");
   const [lionScore, setLionScore] = useState(0);
   const [tigerScore, setTigerScore] = useState(0);
   const [currentQuarter, setQuarter] = useState(0);
+  const [secondOnes, setSecondOnes] = useState(0);
+  const [secondTens, setSecondTens] = useState(0);
+  const [minuteOnes, setMinuteOnes] = useState(0);
+  const [minuteTens, setMinuteTens] = useState(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (secondOnes === 9) {
+        setSecondOnes(0);
+        setSecondTens(secondTens + 1);
+      }
+      else {
+        setSecondOnes(secondOnes + 1);
+      }
+      if ((secondOnes === 9) && (secondTens === 5)) {
+        setSecondOnes(0);
+        setSecondTens(0);
+        setMinuteOnes(minuteOnes + 1);
+      }
+      if (minuteOnes === 9) {
+        setMinuteOnes(0);
+        setMinuteTens(minuteTens + 1);
+      }
+    }, 1000);
+  });
 
   const touchdown = (homeTeam) => {
     if (homeTeam === true) {
@@ -48,7 +74,7 @@ function App() {
 
             <div className="home__score">{lionScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer"><div>{minuteTens}</div><div>{minuteOnes}</div><div>:</div><div>{secondTens}</div><div>{secondOnes}</div></div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{tigerScore}</div>
