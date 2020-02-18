d0461ecc93d7d8b26bd174d0f3170b7a6eb06a1d
Alexander Karren <alexanderkarren@Alexanders-MacBook-Pro.local>
2020-02-18 13:11:23 -0700

    added additional styles

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -10,11 +10,11 @@ class BottomRow extends React.Component {
           <div className="down__value">3</div>
         </div>
         <div className="toGo">
-          <h3 className="toGo__title">To Go</h3>
+          <h3 className="toGo__title">To&nbsp;Go</h3>
           <div className="toGo__value">7</div>
         </div>
         <div className="ballOn">
-          <h3 className="ballOn__title">Ball on</h3>
+          <h3 className="ballOn__title">Ball&nbsp;on</h3>
           <div className="ballOn__value">21</div>
         </div>
         <div className="quarter">
